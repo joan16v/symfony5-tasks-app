6c0afef858140af7b3310d072db9c2a1f19f2d4e
joagido <joagido@yahoo.es>
2019-11-29 14:23:19 +0100

    new symfony

---
+++ src/Controller/MainController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/")
+ */
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", methods={"GET"}, name="app_index")
+     */
+    public function index(Request $request): Response
+    {
+        dump('fdfdf');exit;
+    }
+}
