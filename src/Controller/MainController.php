ce064bc2f18dcae6321dbc05bd0dac1560d0ec6b
joagido <joagido@yahoo.es>
2019-12-02 11:14:10 +0100

    index added

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -26,14 +26,25 @@ class MainController extends AbstractController
     {
         $session = $this->get('session');
         $sessionUser = $session->get('user');
+        $currentYear = date('Y');
+        $year = !empty($request->get('year')) ? $request->get('year') : $currentYear;
+        $currentWeek = $this->getCurrentWeek();
+        $currentWeekValue = $this->getCurrentWeekValue();
+        $week = !empty($request->get('week')) ? $request->get('week') : $currentWeekValue;
 
         if (empty($sessionUser)) {
             return $this->redirectToRoute('app_login');
         }
-dump('gfdgfd');
-        dump($sessionUser);exit;
 
-        return new Response('ggg');
+        return $this->render(
+            'index.html.twig',
+            [
+                'request' => $request,
+                'user' => $sessionUser,
+                'year' => $year,
+                'week' => $week,
+            ]
+        );
     }
 
     /**
@@ -56,6 +67,10 @@ dump('gfdgfd');
 
                 return $this->redirectToRoute('app_index');
             }
+
+            $this->addFlash('notice', 'Login error');
+
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render(
@@ -64,5 +79,33 @@ dump('gfdgfd');
                 'request' => $request,
             ]
         );
+    }
+
+    /**
+     * @Route("/logout", methods={"GET", "POST"}, name="app_logout")
+     */
+    public function logout(Request $request): Response
+    {
+        $session = $this->get('session');
+        $session->remove('user');
+
+        return $this->redirectToRoute('app_index');
+    }
+
+    private function getCurrentWeekValue()
+    {
+        $date = new \DateTime();
+
+        return $date->format("W");
+    }
+
+    private function getCurrentWeek()
+    {
+        $dtmin = new \DateTime("last sunday");
+        $dtmin->modify('+1 day');
+        $dtmax = clone($dtmin);
+        $dtmax->modify('+6 days');
+
+        return $dtmin->format('d/m/Y') . ' - ' . $dtmax->format('d/m/Y');
     }
 }
